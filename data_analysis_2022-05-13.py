e1db4dce4f9b035f8f77a243a496b2dd490997ad
Yeonsu Jung <ynsuju@gmail.com>
2022-05-21 13:50:18 -0400

    Update data_analysis_2022-05-13.py

--- data_analysis_2022-05-13.py
+++ data_analysis_2022-05-13.py
@@ -4,31 +4,28 @@ from matplotlib import pyplot as plt
 # %%
 class data_analyzer:
     def __init__(self,paths):
-        self.zeroing_data = np.loadtxt(paths[0],delimiter=',')
-        self.forward_data1 = np.loadtxt(paths[1],delimiter=',') # 0.1 mm/s
-        self.forward_data2 = np.loadtxt(paths[2],delimiter=',') # 0.3 mm/s
-        self.forward_data3 = np.loadtxt(paths[3],delimiter=',') # 1 mm/s
+        self.pull1 = np.loadtxt(paths[0],delimiter=',')
+        self.pull2 = np.loadtxt(paths[1],delimiter=',')
+        self.push1 = np.loadtxt(paths[2],delimiter=',')
+        self.push2 = np.loadtxt(paths[3],delimiter=',')
 
-        self.backward_data1 = np.loadtxt(paths[4],delimiter=',') # 0.1 mm/s
-        self.backward_data2 = np.loadtxt(paths[5],delimiter=',') # 0.1 mm/s
-        self.backward_data3 = np.loadtxt(paths[6],delimiter=',') # 0.1 mm/s
+        # self.backward_data1 = np.loadtxt(paths[4],delimiter=',') # 0.1 mm/s
+        # self.backward_data2 = np.loadtxt(paths[5],delimiter=',') # 0.1 mm/s
+        # self.backward_data3 = np.loadtxt(paths[6],delimiter=',') # 0.1 mm/s
 
-        self.zeros = np.mean(self.zeroing_data,axis=0)
+        self.zeros = np.array([2.15488281,0.49443359])
 
     def get_zeros(self):
-        self.zeros = np.mean(self.zeroing_data,axis=0)        
+        self.zeros = np.mean(self.zeroing_data,axis=0)
 
     def show_raw_data(self):
         # plt.plot(self.zeroing_data)
         fig, axs = plt.subplots(2)
-        axs[0].plot(self.forward_data1[:,0] - self.zeros[0],label='0.1 mm/s')
-        axs[1].plot(self.backward_data1[:,0] - self.zeros[0])
+        axs[0].plot(self.push1[:,0] - self.zeros[0],label='push')
+        axs[1].plot(self.push2[:,0] - self.zeros[0])
 
-        axs[0].plot(self.forward_data2[:,0] - self.zeros[0],label='0.3 mm/s')
-        axs[1].plot(self.backward_data2[:,0] - self.zeros[0])
-
-        axs[0].plot(self.forward_data3[:,0] - self.zeros[0],label='1 mm/s')
-        axs[1].plot(self.backward_data3[:,0] - self.zeros[0])
+        axs[0].plot(self.pull1[:,0] - self.zeros[0],label='pull')
+        axs[1].plot(self.pull2[:,0] - self.zeros[0])
 
         fig.legend()
 
@@ -101,41 +98,69 @@ class data_analyzer:
         # axs[0].set_ylim((0, 0.35))
         # axs[1].set_ylim((0, 0.35))
         fig.legend()
-    
-    
-
+       
     
 # %%
-exp_id_30mm = ['21-34','21-39','21-55','21-59']
-exp_id_45mm = ['22-10','22-16','22-22','22-26']
-exp_id_60mm = ['16-17','16-32','16-45','16-50']
+exp_id_30mm = ['21-55','21-59','21-34','21-39']
+exp_id_45mm = ['22-22','22-26','22-10','22-16']
+exp_id_60mm = ['16-32','16-37','16-45','16-50']
 exp_id_90mm = ['22-36','22-41','22-46','22-51']
 
 paths_30mm = []
 paths_45mm = []
 paths_60mm = []
 paths_90mm = []
-for i in range(3):
-    paths_30mm.append(f'examples/console/LoadCellLog_2022-04-01_{exp_id_30mm[i]}.csv')
-    paths_45mm.append(f'examples/console/LoadCellLog_2022-04-01_{exp_id_45mm[i]}.csv')
-    paths_60mm.append(f'examples/console/LoadCellLog_2022-04-01_{exp_id_60mm[i]}.csv')
-    paths_90mm.append(f'examples/console/LoadCellLog_2022-04-01_{exp_id_90mm[i]}.csv')
-
-# %%
-da = data_analyzer(paths_60mm)
-da.get_zeros()
-da.show_raw_data()
-da.show_data1()
+for i in range(4):
+    paths_30mm.append(f'examples/console/LoadCellLog_2022-05-12_{exp_id_30mm[i]}.csv')
+    paths_45mm.append(f'examples/console/LoadCellLog_2022-05-12_{exp_id_45mm[i]}.csv')
+    paths_60mm.append(f'examples/console/LoadCellLog_2022-05-12_{exp_id_60mm[i]}.csv')
+    paths_90mm.append(f'examples/console/LoadCellLog_2022-05-12_{exp_id_90mm[i]}.csv')
 
 # %%
 da30 = data_analyzer(paths_30mm)
-da30.show_data1()
+da30.show_raw_data()
 da45 = data_analyzer(paths_45mm)
-da45.show_data1()
+da45.show_raw_data()
 da60 = data_analyzer(paths_60mm)
-da60.show_data1()
+da60.show_raw_data()
 da90 = data_analyzer(paths_90mm)
-da90.show_data1()
+da90.show_raw_data()
+# %%
+from scipy.fft import fft, ifft
+# %%
+import numpy as np
+
+# %%
+t = np.arange(0,1000*0.25,0.25)
+
+# %%
+xx = da90.pull2[:,0]
+sp = np.fft.fft(xx - np.mean(xx))
+freq = np.fft.fftfreq(1000,0.25)
+
+xx = da90.pull2[:,0]
+sp = np.fft.fft(xx - np.mean(xx))
+freq = np.fft.fftfreq(1000,0.25)
+
+xx = da90.pull2[:,0]
+sp = np.fft.fft(xx - np.mean(xx))
+freq = np.fft.fftfreq(1000,0.25)
+
+xx = da90.pull2[:,0]
+sp = np.fft.fft(xx - np.mean(xx))
+freq = np.fft.fftfreq(1000,0.25)
+
+plt.plot(freq,sp.real)
+# %%
+t = np.arange(256)
+freq = np.fft.fftfreq(t.shape[-1])
+
+
+freq = np.fft.fftfreq(da30.pull1[:,0])
+plt.plot(freq, sp.real, freq, sp.imag)
+
+plt.show()
+
 # %%
 da30 = data_analyzer(paths_30mm)
 da30.show_data1()
@@ -166,7 +191,7 @@ plt.plot(test_data)
 
 # %%
 plt.plot(zeroing_data)
-average = np.mean(zeroing_data,axis=0)
+average = np.mean(zeroing_data,axis=0) 
 
 plt.plot(forward_data)
 plt.plot(backward_data)
